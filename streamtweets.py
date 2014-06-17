9b9ed7a21c846be568d8afc2ede96f1a60d4139e
Gaurav Yadav <gaurav.dev.iiitm@gmail.com>
2014-06-17 12:42:00 +0530

    Update streamtweets.py

--- streamtweets.py
+++ streamtweets.py
@@ -2,10 +2,10 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-ckey='6zSyLNhbmD1O68uNhZtF16ifJ'
-csecret='CbI4Ew3t4HLnkpGjBP7YC1Bm31HxTDJ9rQiERyiy37hGSdALyE'
+ckey='6zSyLNhbmD1O68uNhfJ'
+csecret='CbI4Ew3t4HLnkpGjBP7YC1Bm31HxTDrQiERyiy37hGSdALyE'
 atoken='298398033-jU0Q9NvuD2B0cmkiX97zgzoVig1Q3tgIgFSmj5Vx'
-asecret='mG6uiNPzu91V2I6KTCInm8Mh6NKorzGw5od4TNXknVd0f'
+asecret='mG6uiNPzu91V2I6KTCInm8Mh6NK4TNXknVd0f'
 
 class listener(StreamListener):
 	def on_data(self,data):
